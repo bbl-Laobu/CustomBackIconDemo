bee6f27eaeb06a5da3de5bd697c5c57a268a291b
bbl-Laobu <bernardblanckaert@yahoo.co.uk>
2017-06-18 18:00:02 +0200

    Appcomp Ready, waiting on Xamarin Fix

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,39 +11,13 @@ using Android.OS;
 namespace CustomBackIconDemo.Droid
 {
 	// -------------------------------------------------------------------------
-	// APPCOMP CODE - Instructions
-	// 1. uncomment the code below 
-	// 2. Comment out the 'NON APPCOMP CODE' below
-	// 3. Uncomment the 'APPCOMP CODE' in NavigationPageRendererDroid
-	// 4. Comment out the 'NON APPCOMP CODE' in NavigationPageRendererDroid
-	//
-	// -------------------------------------------------------------------------
-	
-	[Activity(Label = "CustomBackIconDemo.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(bundle);
-
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-
-            LoadApplication(new App());
-        }
-    }
-
-	// -------------------------------------------------------------------------
 	// NON APPCOMP CODE - INSTRUCTIONS
-	// 1. uncomment th code below 
-	// 2. Comment out the 'APPCOMP CODE' above
+	// 1. uncomment the code below 
+	// 2. Comment out the 'APPCOMP CODE'
 	// 3. Uncomment the 'NON APPCOMP CODE' in NavigationPageRendererDroid
 	// 4. Comment out the 'APPCOMP CODE' in NavigationPageRendererDroid
 	// -------------------------------------------------------------------------
-	/*
-    [Activity(Label = "CustomBackIcon.Droid", 
+	[Activity(Label = "CustomBackIcon.Droid", 
               Icon = "@drawable/icon", 
               Theme = "@android:style/Theme.Holo.Light",
               MainLauncher = true, 
@@ -54,6 +28,35 @@ namespace CustomBackIconDemo.Droid
         protected override void OnCreate(Bundle bundle)
         {
             
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+
+
+	// IMPORTANT NOTE: 
+	// The APPCOMP solution can not work at this stage due to a bug in the Xamarin Forms Base class NavigationPageRenderer
+	// 2017-06-18 waiting on a Pull REquest to be approved and released so we can activate this code: see https://bugzilla.xamarin.com/show_bug.cgi?id=57578
+	// -------------------------------------------------------------------------
+	// APPCOMP CODE - Instructions
+	// 1. uncomment the code 
+	// 2. Comment out the 'NON APPCOMP CODE'
+	// 3. Uncomment the 'APPCOMP CODE' in NavigationPageRendererDroid
+	// 4. Comment out the 'NON APPCOMP CODE' in NavigationPageRendererDroid
+	//
+	// -------------------------------------------------------------------------
+	/*
+    [Activity(Label = "CustomBackIconDemo.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
--- Droid/NavigationPageRendererDroid.cs
+++ Droid/NavigationPageRendererDroid.cs
@@ -5,131 +5,28 @@ using Xamarin.Forms;
 using Android.Graphics.Drawables;
 using CustomBackIconDemo.Pages;
 
+// IMPORTANT NOTE: 
+// The APPCOMP solution can not work at this stage due to a bug in the Xamarin Forms Base class NavigationPageRenderer
+// 2017-06-18 waiting on a Pull REquest to be approved and released so we can activate this code: see https://bugzilla.xamarin.com/show_bug.cgi?id=57578
+// The code is ready for when the fix has been released
+
+
+// -------------------------------------------------------------------------
+// To Switch between APPCOMP CODE and NON APPCOMP CODE Instructions
+// simple uncomment and comment where indicated.
+// ------------------------------------------------------------------------- 
+
 using Xamarin.Forms.Platform.Android.AppCompat; // APPCOMP CODE 
 using Xamarin.Forms.Platform.Android;
-//using Xamarin.Forms.Platform.Android; // NON APPCOMP CODE
 
-// -------------------------------------------------------------------------
-// APPCOMP CODE - Instructions
-// 1. uncomment the code below 
-// 2. Comment out the 'NON APPCOMP CODE' below
-// 3. Uncomment the 'APPCOMP CODE' in NavigationPageRendererDroid
-// 4. Comment out the 'NON APPCOMP CODE' in NavigationPageRendererDroid
-//
-// -------------------------------------------------------------------------
-[assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomBackIconDemo.Droid.NavigationPageRendererDroid))]
+
+// [assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomBackIconDemo.Droid.NavigationPageRendererDroid))] // APPCOMP
+[assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomBackIconDemo.Droid.NavigationPageRendererDroid))] // NON APPCOMP
 namespace CustomBackIconDemo.Droid
 {
-    public class NavigationPageRendererDroid : NavigationPageRenderer
-    {
-        static ActionBar actionBar;
 
-        public NavigationPageRendererDroid() : base()
-        {
-            actionBar = ((Activity)Context).ActionBar;
-        }
-
-
-        protected new Task<bool> PopViewAsync(Page page, bool animated = true) 
-		{
-			var retVal = base.PopViewAsync(page, animated);
-
-			var returnPage = ((INavigationPageController)base.Element).StackCopy.ToArray()[1];
-			if (returnPage != null)
-			{
-				SetHeaderIcons(returnPage);
-			}
-
-			return retVal;
-		}
-
-		public new Task<bool> PushViewAsync(Page page, bool animated = true)
-		{
-			var retVal = base.PushViewAsync(page, animated);
-			SetHeaderIcons(page);
-			return retVal;
-		}
-
-		void SetHeaderIcons(Page view)
-		{
-			if (view is INavigationActionBarConfig incomingPage)
-			{
-				// select the correct back icon to show
-				switch (incomingPage.BackButtonStyle)
-				{
-					case 0: // 0=default 
-						SetDefaultBackButton();
-						break;
-
-					case 1: // 1=Hide 
-						actionBar.SetHomeAsUpIndicator(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide back arrow
-						actionBar.SetIcon(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide Icon
-						view.Title = "";
-						break;
-
-					case 2: // 2=Image & Text 
-						actionBar.SetHomeAsUpIndicator(CustomBackIconDemo.Droid.Resource.Drawable.backman_icon); // Show custom image
-						actionBar.SetIcon(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide Icon
-						break;
-
-					case 3: // 3=Image only
-						actionBar.SetHomeAsUpIndicator(CustomBackIconDemo.Droid.Resource.Drawable.backman_icon); // Show custom image 
-						actionBar.SetIcon(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide Icon
-						view.Title = "";
-						break;
-
-					case 4: // 4=Text only
-						actionBar.SetHomeAsUpIndicator(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide back arrow
-						actionBar.SetIcon(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide Icon
-						view.Title = " " + view.Title;
-						break;
-
-					case 6: // 2=Image & Text 
-						actionBar.SetHomeAsUpIndicator(CustomBackIconDemo.Droid.Resource.Drawable.backman_icon); // Show custom image
-						actionBar.SetIcon(CustomBackIconDemo.Droid.Resource.Drawable.icon); // set Brand icon
-						break;
-
-					case 7: // 6=Hide Brand Icon
-						actionBar.SetIcon(new ColorDrawable(Color.Transparent.ToAndroid())); // Hide Icon
-						break;
-
-					default:
-						SetDefaultBackButton();
-						break;
-				}
-			}
-			else
-			{
-				SetDefaultBackButton(); // set default behavior back 
-			}
-
-		}
-
-
-		// Sets the default Back Button 
-		// -------------------------------
-		void SetDefaultBackButton()
-		{
-			actionBar.SetHomeAsUpIndicator(null);
-			actionBar.SetIcon(CustomBackIconDemo.Droid.Resource.Drawable.icon);
-		}
-    }
-
-
-}
-
-/*
-// -------------------------------------------------------------------------
-// NON APPCOMP CODE - INSTRUCTIONS
-// 1. uncomment the code below 
-// 2. Comment out the 'APPCOMP CODE' above
-// 3. Uncomment the 'NON APPCOMP CODE' in MainActivity
-// 4. Comment out the 'APPCOMP CODE' in MainActivity
-// -------------------------------------------------------------------------
-[assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomBackIconDemo.Droid.NavigationPageRendererDroid))]
-namespace CustomBackIconDemo.Droid
-{
-	public class NavigationPageRendererDroid : NavigationRenderer
+    //public class NavigationPageRendererDroid : NavigationPageRenderer // APPCOMP
+    public class NavigationPageRendererDroid : NavigationRenderer // NON APPCOMP
     {
         static ActionBar actionBar;
 
@@ -216,7 +113,6 @@ namespace CustomBackIconDemo.Droid
 
         }
 
-
         // Sets the default Back Button 
         // -------------------------------
         void SetDefaultBackButton()
@@ -224,7 +120,5 @@ namespace CustomBackIconDemo.Droid
             actionBar.SetHomeAsUpIndicator(null);
             actionBar.SetIcon(CustomBackIconDemo.Droid.Resource.Drawable.icon);
         }
-
     }
 }
-*/
